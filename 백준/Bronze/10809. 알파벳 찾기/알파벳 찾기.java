fc3371b530c0cfd5b4aabaa9c17ac55a6859c0ca
Mirage <86884419+ttap112@users.noreply.github.com>
2024-04-09 10:18:07 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];
+        for (int i = 0; i < 26; i++) {
+            arr[i] = -1;  
+        }
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (arr[(int)c - 97] == -1) {  
+                arr[(int)c - 97] = i;  
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
