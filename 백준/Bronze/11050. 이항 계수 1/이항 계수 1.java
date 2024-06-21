b10fa718b77d78f19bc79e6fa9614d4e455f58ab
Mirage <86884419+ttap112@users.noreply.github.com>
2024-06-21 23:50:18 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+ 
+		System.out.println(BC(N, K));
+		
+	}
+	
+	static int BC(int n, int k) {
+		if(n == k || k == 0) {
+			return 1;
+		}
+
+		return BC(n - 1, k - 1) + BC(n - 1, k);
+	}
+}
